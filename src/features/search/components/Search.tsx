99f0a3f00ca398f735db4515c51d1cb3cc95b8ac
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-11-19 22:27:46 +0400

    feat: create search component

--- src/features/search/components/Search.tsx
+++ src/features/search/components/Search.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
+import { SearchClose } from "../assets/SearchClose";
+import { SearchIcon } from "../assets/SearchIcon";
+
 import styles from "./styles/Search.module.scss";
 
 export const Search = () => {
-  return <div className={styles.container}>Search</div>;
+  const [search, setSearch] = useState<string>("");
+
+  return (
+    <div className={styles.container}>
+      <form>
+        <div className={styles.icon}>
+          <SearchIcon />
+        </div>
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Search Twitter"
+        />
+        {search && (
+          <button onClick={() => setSearch("")} className={styles.close}>
+            <SearchClose />
+          </button>
+        )}
+      </form>
+    </div>
+  );
 };
