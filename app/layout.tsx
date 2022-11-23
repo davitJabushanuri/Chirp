5fe5ad5b4930e70dd211917a1fc9ff513db7d552
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-11-23 06:07:45 +0400

    chore: implement theme switching

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { useState } from "react";
+
 import { Aside } from "@/components/layout/aside";
 import { Header } from "@/features/header";
 import { MobileNavbar } from "@/features/navbar";
 import { TweetButton } from "@/features/sidebar";
 import { Sidebar } from "@/features/sidebar";
-import "./globals.css";
+
 import "./layout.scss";
 
 export default function RootLayout({
@@ -12,8 +14,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [theme, setTheme] = useState("light");
+
   return (
-    <html lang="en">
+    <html className={theme} lang="en">
       <body>
         <div className="layout">
           <MobileNavbar />
@@ -24,6 +28,15 @@ export default function RootLayout({
           <main>
             <Header />
             {children}
+            <button
+              onClick={() =>
+                setTheme((prev) => {
+                  return prev === "light" ? "dark" : "light";
+                })
+              }
+            >
+              switch theme
+            </button>
           </main>
           <Aside />
         </div>
