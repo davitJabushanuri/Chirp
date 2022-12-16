03e96b7519bbef94b523ae0ee4f2808bad8b5bfc
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-12-16 23:30:26 +0400

    feat: create signup form

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -1,3 +1,40 @@
+import { AppleLogo } from "../assets/apple-logo";
+import { GoogleLogo } from "../assets/google-logo";
+
+import styles from "./styles/register-form.module.scss";
+
 export const RegisterForm = () => {
-  return <div>Register</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>New to Twitter?</h1>
+      <p className={styles.description}>
+        Sign up now to get your own personalized timeline!
+      </p>
+
+      <div className={styles.buttons}>
+        <button className={styles.google}>
+          <span className={styles.icon}>
+            <GoogleLogo />
+          </span>
+          <span className={styles.text}>Sign up with Google</span>
+        </button>
+
+        <button className={styles.apple}>
+          <span className={styles.icon}>
+            <AppleLogo />
+          </span>
+          <span className={styles.text}>Sign up with Apple</span>
+        </button>
+
+        <button className={styles.email}>
+          <span className={styles.text}>Sign up with Email</span>
+        </button>
+      </div>
+
+      <div className={styles.terms}>
+        By signing up, you agree to the <a href="/">Terms of Service</a> and{" "}
+        <a href="/">Privacy Policy</a>, including <a href="/">Cookie Use</a>.
+      </div>
+    </div>
+  );
 };
