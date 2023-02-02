32b424e4d4d360bbb03ab8e7e250f0004d728d37
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-02-03 01:14:32 +0400

    perf: refactor middleware to also check for secure token

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,21 +4,24 @@ import type { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   const token = req.cookies.get("next-auth.session-token");
+  const secureToken = req.cookies.get("__Secure-next-auth.session-token");
+
+  const isAuthorized = token || secureToken;
 
   if (
     req.nextUrl.pathname.startsWith(
       "/home" || "/notifications" || "/messages" || "/bookmarks",
     ) &&
-    !token
+    !isAuthorized
   ) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  if (url.pathname === "/" && token) {
+  if (url.pathname === "/" && isAuthorized) {
     return NextResponse.redirect(new URL("/home", req.url));
   }
 
-  if (req.nextUrl.pathname.startsWith("/auth/signin") && token) {
+  if (req.nextUrl.pathname.startsWith("/auth/signin") && isAuthorized) {
     return NextResponse.redirect(new URL("/home", req.url));
   }
 
