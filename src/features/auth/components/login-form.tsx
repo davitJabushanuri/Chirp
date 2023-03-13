8c7dce2647d53678e55127d92b995cf54d4c2439
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-03-13 19:05:50 +0400

    refactor: auth-modal styles

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -11,59 +11,57 @@ import styles from "./styles/login-form.module.scss";
 export const LoginForm = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.loginForm}>
-        <h1 className={styles.title}>Sign in to Twitter</h1>
+      <h1 className={styles.title}>Sign in to Twitter</h1>
 
+      <div className={styles.buttonContainer}>
+        <button onClick={() => signIn("google")}>
+          <span className={styles.icon}>
+            <GoogleLogo />
+          </span>
+          <span className={styles.text}>Sign in with Google</span>
+        </button>
+      </div>
+
+      <div className={styles.buttonContainer}>
+        <button onClick={() => signIn("apple")}>
+          <span className={styles.icon}>
+            <AppleLogo />
+          </span>
+          <span className={styles.text}>Sign in with Apple</span>
+        </button>
+      </div>
+
+      <div className={styles.divider}>
+        <span className={styles.line}></span>
+        <span className={styles.text}>or</span>
+        <span className={styles.line}></span>
+      </div>
+
+      <form>
+        <div className={styles.inputContainer}>
+          <label htmlFor="name">
+            <input
+              type="text"
+              id="name"
+              placeholder="Phone, email, or username"
+            />
+            <span>Phone, email, or username</span>
+          </label>
+        </div>
         <div className={styles.buttonContainer}>
-          <button onClick={() => signIn("google")}>
-            <span className={styles.icon}>
-              <GoogleLogo />
-            </span>
-            <span className={styles.text}>Sign in with Google</span>
-          </button>
+          <button className={styles.submit}>Next</button>
         </div>
+      </form>
+      <div className={`${styles.forgotPassword} ${styles.buttonContainer}`}>
+        <button>
+          <Link href={`/`}>Forgot password?</Link>
+        </button>
+      </div>
 
-        <div className={styles.buttonContainer}>
-          <button onClick={() => signIn("apple")}>
-            <span className={styles.icon}>
-              <AppleLogo />
-            </span>
-            <span className={styles.text}>Sign in with Apple</span>
-          </button>
-        </div>
+      <div className={styles.signUp}>
+        <span>Don’t have an account?</span>
 
-        <div className={styles.divider}>
-          <span className={styles.line}></span>
-          <span className={styles.text}>or</span>
-          <span className={styles.line}></span>
-        </div>
-
-        <form>
-          <div className={styles.inputContainer}>
-            <label htmlFor="name">
-              <input
-                type="text"
-                id="name"
-                placeholder="Phone, email, or username"
-              />
-              <span>Phone, email, or username</span>
-            </label>
-          </div>
-          <div className={styles.buttonContainer}>
-            <button className={styles.submit}>Next</button>
-          </div>
-        </form>
-        <div className={`${styles.forgotPassword} ${styles.buttonContainer}`}>
-          <button>
-            <Link href={`/`}>Forgot password?</Link>
-          </button>
-        </div>
-
-        <div className={styles.signUp}>
-          <span>Don’t have an account?</span>
-
-          <Link href={`/`}>Sign up</Link>
-        </div>
+        <Link href={`/`}>Sign up</Link>
       </div>
     </div>
   );
