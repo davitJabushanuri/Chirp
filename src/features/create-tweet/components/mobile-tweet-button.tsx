ea75f00731356fb653dc18f02d9bdebea500da88
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-03-15 16:18:50 +0400

    refactor: hide tweet button if user is not logged in

--- src/features/create-tweet/components/mobile-tweet-button.tsx
+++ src/features/create-tweet/components/mobile-tweet-button.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useSession } from "next-auth/react";
+
 import { useCreateTweetModal } from "@/stores/use-create-tweet-modal";
 
 import { PenIcon } from "../assets/pen-icon";
@@ -5,7 +8,10 @@ import { PenIcon } from "../assets/pen-icon";
 import styles from "./styles/mobile-tweet-button.module.scss";
 
 export const MobileTweetButton = () => {
+  const { data: session } = useSession();
   const openModal = useCreateTweetModal((state) => state.openModal);
+
+  if (!session) return null;
 
   return (
     <button onClick={() => openModal()} className={styles.container}>
