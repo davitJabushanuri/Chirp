903e5bbd0980234fcedb24750797fcae1626a0a1
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-03-20 21:15:35 +0400

    chore: create designs directory to separate design files from other files
    chore: create avatar component in designs folder

---
+++ src/components/designs/avatar/avatar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+import styles from "./styles/avatar.module.scss";
+
+export const Avatar = ({
+  userImage,
+  width = 46,
+  height = 46,
+}: {
+  userImage: string | undefined;
+  width: number;
+  height: number;
+}) => {
+  return (
+    <Image
+      className={styles.avatar}
+      src={userImage || `/user_placeholder.png`}
+      alt="profile picture"
+      width={width}
+      height={height}
+    />
+  );
+};
