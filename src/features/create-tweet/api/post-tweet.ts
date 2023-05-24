2375aaaa327b16a9d3316042d43406d1417ade9a
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-05-24 21:55:02 +0400

    refactor: optimize data sent to server by conditionally including properties in the tweet object

--- src/features/create-tweet/api/post-tweet.ts
+++ src/features/create-tweet/api/post-tweet.ts
@@ -19,13 +19,20 @@ export const postTweet = async ({
   in_reply_to_status_id?: string | null;
   quoted_tweet_id?: string | null;
 }) => {
+  const tweet = {
+    text,
+    author_id: userId,
+    // if no in_reply_to_screen_name, don't send it
+    ...(in_reply_to_screen_name && { in_reply_to_screen_name }),
+    // if no in_reply_to_status_id, don't send it
+    ...(in_reply_to_status_id && { in_reply_to_status_id }),
+    // if no quoted_tweet_id, don't send it
+    ...(quoted_tweet_id && { quoted_tweet_id }),
+  };
+
   try {
-    const { data } = await axios.post(`/api/tweets/create`, {
-      text,
-      userId,
-      in_reply_to_screen_name,
-      in_reply_to_status_id,
-      quoted_tweet_id,
+    const { data } = await axios.post(`/api/tweets`, {
+      tweet,
     });
 
     if (files.length > 0) {
