8693afd7de40e1cd16fa338db936a9d5400a9151
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-05-27 03:49:44 +0400

    refactor: move retweets api route to app directory

---
+++ app/api/tweets/retweets/route.ts
@@ -0,0 +1,55 @@
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+import { prisma } from "@/lib/prisma";
+
+export async function POST(request: Request) {
+  const { tweet_id, user_id } = await request.json();
+
+  const retweetSchema = z
+    .object({
+      tweet_id: z.string().cuid(),
+      user_id: z.string().cuid(),
+    })
+    .strict();
+
+  const zod = retweetSchema.safeParse({ tweet_id, user_id });
+
+  if (!zod.success) {
+    return NextResponse.json(
+      {
+        message: "Invalid request body",
+        error: zod.error.formErrors,
+      },
+      { status: 400 },
+    );
+  }
+
+  try {
+    const retweet = await prisma.retweet.findFirst({
+      where: {
+        tweet_id,
+        user_id,
+      },
+    });
+
+    if (retweet) {
+      await prisma.retweet.delete({
+        where: {
+          id: retweet.id,
+        },
+      });
+      return NextResponse.json({ message: "Tweet un retweeted" });
+    } else {
+      await prisma.retweet.create({
+        data: {
+          tweet_id,
+          user_id,
+        },
+      });
+      return NextResponse.json({ message: "Tweet retweeted" });
+    }
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
