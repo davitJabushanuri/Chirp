93ce1c739dcee1aea134e167ef371f3efc385c4d
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-05-27 19:53:13 +0400

    refactor: move hashtags api route to app directory

---
+++ app/api/hashtags/route.ts
@@ -0,0 +1,74 @@
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  try {
+    const hashtags = await prisma.hashtag.findMany({
+      take: 10,
+      orderBy: {
+        score: "desc",
+      },
+    });
+    return NextResponse.json(hashtags, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  const { hashtags } = await request.json();
+
+  const hashtagsSchema = z.array(z.string());
+
+  const zod = hashtagsSchema.safeParse(hashtags);
+
+  if (!zod.success) {
+    return NextResponse.json({ error: zod.error }, { status: 400 });
+  }
+
+  try {
+    for (const hashtag of hashtags) {
+      const hashtagExists = await prisma.hashtag.findUnique({
+        where: {
+          hashtag: hashtag.toLowerCase(),
+        },
+      });
+
+      if (hashtagExists) {
+        await prisma.hashtag.update({
+          where: {
+            hashtag: hashtag.toLowerCase(),
+          },
+          data: {
+            score: {
+              increment: 1,
+            },
+          },
+        });
+      } else {
+        await prisma.hashtag.create({
+          data: {
+            text: hashtag,
+            hashtag: hashtag.toLowerCase(),
+          },
+        });
+      }
+    }
+
+    return NextResponse.json(
+      {
+        message: "Hashtag(s) created",
+      },
+      { status: 200 },
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      { status: 500 },
+    );
+  }
+}
--- src/features/explore/api/retrieve-hashtags-from-tweet.ts
+++ src/features/explore/api/retrieve-hashtags-from-tweet.ts
@@ -1,6 +1,4 @@
 export const retrieveHashtagsFromTweet = (text: string): string[] | null => {
-  // make all hashtags lowercase
-
   const hashtags = text.match(/#\w+/gi);
   return hashtags ? hashtags.map((hashtag) => hashtag.slice(1)) : null;
 };
