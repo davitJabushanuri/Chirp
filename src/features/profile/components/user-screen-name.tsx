b56272582c88be177749221db4d568d53764c8d7
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-07-15 19:18:41 +0400

    refactor: user-screen-name component

--- src/features/profile/components/user-screen-name.tsx
+++ src/features/profile/components/user-screen-name.tsx
@@ -1,25 +1,9 @@
-import { useRouter } from "next/navigation";
-
 import styles from "./styles/user-screen-name.module.scss";
 
 export const UserScreenName = ({
-  userId,
   screenName,
 }: {
-  userId: string | undefined;
   screenName: string | undefined;
 }) => {
-  const router = useRouter();
-
-  return (
-    <button
-      onClick={(e) => {
-        e.stopPropagation();
-        router.push(`/${userId}`);
-      }}
-      className={styles.container}
-    >
-      <span className={styles.username}>@{screenName}</span>
-    </button>
-  );
+  return <span className={styles.container}>@{screenName}</span>;
 };
