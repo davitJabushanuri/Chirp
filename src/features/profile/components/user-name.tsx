fccaa111f47ceca1aac10bb3fab45d688494a5da
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-07-15 19:16:51 +0400

    refactor: user-name component

--- src/features/profile/components/user-name.tsx
+++ src/features/profile/components/user-name.tsx
@@ -1,34 +1,18 @@
-import { useRouter } from "next/navigation";
-
 import { VerifiedIcon } from "@/assets/verified-icon";
 
 import styles from "./styles/user-name.module.scss";
 
 export const UserName = ({
-  userId,
   name,
   isVerified = false,
 }: {
-  userId: string | undefined;
   name: string | undefined;
-  isVerified: boolean | undefined;
+  isVerified?: boolean | undefined;
 }) => {
-  const router = useRouter();
-
   return (
-    <button
-      onClick={(e) => {
-        e.stopPropagation();
-        router.push(`/${userId}`);
-      }}
-      className={styles.container}
-    >
-      <span className={styles.name}>{name}</span>
-      {isVerified && (
-        <span className={styles.verified}>
-          <VerifiedIcon />
-        </span>
-      )}
-    </button>
+    <span className={styles.container}>
+      {name && name}
+      {isVerified && <VerifiedIcon />}
+    </span>
   );
 };
