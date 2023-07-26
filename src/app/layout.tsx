177dfc5b0303a8a742f17eb1d71784d893a53fda
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-07-26 16:48:37 +0400

    refactor: get rid of client-layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,20 @@
 import { cookies } from "next/headers";
+import { ToastContainer, Slide } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 import "./styles/layout.scss";
+import { Aside } from "@/features/aside";
+import { AuthModalTrigger, JoinTwitterModal } from "@/features/auth";
+import { CreateTweetModal, MobileTweetButton } from "@/features/create-tweet";
+import { HamburgerMenu, MobileNavbar } from "@/features/navbar";
+import { Sidebar } from "@/features/sidebar";
+import { InspectTweetImageModal } from "@/features/tweets";
 import NextAuthProvider from "@/utils/next-auth-provider";
+import ReactQueryWrapper from "@/utils/react-query";
 
-import { ClientLayout } from "./client-layout";
+import styles from "./styles/toast.module.scss";
+import "./styles/layout.scss";
 
 export const revalidate = 0;
 
@@ -24,9 +35,40 @@ export default async function RootLayout({
       } ${fontSize?.value ?? "font-size-md"}`}
       lang="en"
     >
-      <NextAuthProvider>
-        <ClientLayout>{children}</ClientLayout>
-      </NextAuthProvider>
+      <body suppressHydrationWarning={true}>
+        <NextAuthProvider>
+          <ReactQueryWrapper>
+            <div className="layout">
+              <MobileNavbar />
+
+              <MobileTweetButton />
+
+              <Sidebar />
+
+              <main>{children}</main>
+
+              <Aside />
+
+              <ToastContainer
+                position="bottom-center"
+                autoClose={4000}
+                hideProgressBar={true}
+                transition={Slide}
+                closeButton={false}
+                closeOnClick={false}
+                className={styles.container}
+                toastClassName={styles.toast}
+              />
+
+              {<CreateTweetModal />}
+              {<HamburgerMenu />}
+              {<AuthModalTrigger />}
+              {<JoinTwitterModal />}
+              {<InspectTweetImageModal />}
+            </div>
+          </ReactQueryWrapper>
+        </NextAuthProvider>
+      </body>
     </html>
   );
 }
