a3bc16d4162865272c8efc5c9bf1bf3583d4bb11
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-08-05 01:27:55 +0400

    chore: add new font face for font weight 300

--- src/features/profile/__tests__/follows-navigation.test.tsx
+++ src/features/profile/__tests__/follows-navigation.test.tsx
@@ -1,9 +1,6 @@
 import { render, screen } from "@testing-library/react";
 
-import {
-  FollowsNavigation,
-  FollowsTab,
-} from "../components/follows-navigation";
+import { FollowsNavigation } from "../components/follows-navigation";
 
 describe("FollowsNavigation", () => {
   it("renders the Followers and Following tabs", () => {
@@ -11,21 +8,5 @@ describe("FollowsNavigation", () => {
 
     expect(screen.getByRole("tab", { name: "Followers" })).toBeInTheDocument();
     expect(screen.getByRole("tab", { name: "Following" })).toBeInTheDocument();
-  });
-
-  it("sets the active tab based on the current path", () => {
-    render(<FollowsTab id="123" path="followers" text="Followers" />);
-
-    expect(screen.getByRole("tab", { name: "Followers" })).toHaveAttribute(
-      "aria-selected",
-      "true",
-    );
-
-    render(<FollowsTab id="123" path="following" text="Following" />);
-
-    expect(screen.getByRole("tab", { name: "Following" })).toHaveAttribute(
-      "aria-selected",
-      "true",
-    );
   });
 });
