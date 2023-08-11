c9dad5e7e4ba09e6033c6fc05afe6b8ca51fb932
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-08-11 23:21:30 +0400

    refactor: tweet-header component to display heading dynamically

--- src/features/header/components/tweet-header.tsx
+++ src/features/header/components/tweet-header.tsx
@@ -2,11 +2,11 @@ import { BackButton } from "@/components/elements/back-button";
 
 import styles from "./styles/tweet-header.module.scss";
 
-export const TweetHeader = () => {
+export const TweetHeader = ({ heading = "Tweet" }: { heading?: string }) => {
   return (
     <div className={styles.container}>
       <BackButton />
-      <h2 className={styles.title}>Tweet</h2>
+      <h2 className={styles.title}>{heading}</h2>
     </div>
   );
 };
