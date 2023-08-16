24d15c46782e7b3d7da6a54815b8da191dc36e7e
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-08-16 22:24:53 +0400

    refactor: profile-navigation

--- src/features/profile/components/profile-navigation.tsx
+++ src/features/profile/components/profile-navigation.tsx
@@ -1,13 +1,13 @@
+"use client";
+import { usePathname } from "next/navigation";
+
 import { NavigationTab } from "./navigation-tab";
 import styles from "./styles/profile-navigation.module.scss";
 
-export const ProfileNavigation = ({
-  pathname,
-  id,
-}: {
-  pathname: string | null;
-  id: string | null;
-}) => {
+export const ProfileNavigation = () => {
+  const pathname = usePathname();
+  const id = pathname?.split("/")[1] || "";
+
   return (
     <nav aria-label="Profile timelines" aria-live="polite">
       <div className={styles.container} role="tablist">
