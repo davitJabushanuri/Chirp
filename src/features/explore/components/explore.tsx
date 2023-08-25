bbb04b98028f6235dc47eab1410d46022d6ef0eb
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-08-25 20:50:21 +0400

    feat: create explore-content component

--- src/features/explore/components/explore.tsx
+++ src/features/explore/components/explore.tsx
@@ -1,46 +1,5 @@
-import { LoadingSpinner } from "@/components/elements/loading-spinner";
-import { TryAgain } from "@/components/elements/try-again";
-import { Trends } from "@/features/trends";
-import { InfiniteTweets, useTweets } from "@/features/tweets";
-
-import styles from "./styles/explore.module.scss";
+import { ExploreContent } from "./explore-content";
 
 export const Explore = () => {
-  const {
-    data: tweets,
-    isLoading,
-    isError,
-    isSuccess,
-    isFetchingNextPage,
-    fetchNextPage,
-    hasNextPage,
-  } = useTweets({});
-
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
-
-  if (isError) {
-    return <TryAgain />;
-  }
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.trends}>
-        <Trends title={`Trends for you`} />
-      </div>
-
-      <div className={styles?.scores}></div>
-
-      <div className={styles.tweets}>
-        <InfiniteTweets
-          tweets={tweets}
-          isSuccess={isSuccess}
-          isFetchingNextPage={isFetchingNextPage}
-          fetchNextPage={fetchNextPage}
-          hasNextPage={hasNextPage}
-        />
-      </div>
-    </div>
-  );
+  return <ExploreContent />;
 };
