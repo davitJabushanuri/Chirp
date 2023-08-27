6da4721ea64135ab709ef5f386824c3d2a9b6f3a
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-08-27 22:05:43 +0400

    refactor: session-owner-modal to use new modal component

--- src/features/auth/components/session-owner-modal.tsx
+++ src/features/auth/components/session-owner-modal.tsx
@@ -1,32 +1,22 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
+"use client";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
-
-import { useAuthModal } from "@/stores/use-auth-modal";
 
 import styles from "./styles/session-owner-modal.module.scss";
 
 export const SessionOwnerModal = () => {
   const { data: session } = useSession();
-  const closeUserModal = useAuthModal((state) => state.closeUserModal);
+
+  if (!session) return null;
 
   return (
-    <div onClick={() => closeUserModal()} className={styles.container}>
-      <div className={styles.modalContainer}>
-        {session && (
-          <div className={styles.modal}>
-            <button className={styles.addAccount}>
-              <Link href={`/auth/signin`}>Add an existing account</Link>
-            </button>
-            <button>
-              <Link href={`/auth/signout`}>
-                Log out @{session?.user?.email.split("@")[0]}
-              </Link>
-            </button>
-          </div>
-        )}
-      </div>
+    <div className={styles.container} role="group">
+      <Link href={`/auth/signin`} role="menuitem">
+        Add an existing account
+      </Link>
+      <Link href={`/auth/signout`} role="menuitem">
+        Log out @{session?.user?.email.split("@")[0]}
+      </Link>
     </div>
   );
 };
