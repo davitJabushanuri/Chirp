2f6a2f6fe7e29a90ffad3cf8bcd35e300702c369
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-09-01 02:46:19 +0400

    refactor: get rid of depreciated auth-modal component

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,5 +1,4 @@
 export * from "./components/auth-modal-trigger";
-export * from "./components/auth-modal";
 export * from "./components/login-form";
 export * from "./components/register-form";
 export * from "./components/signout-modal";
