b1716060e36cd436f793be834246db12198ce73b
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-09-13 22:11:10 +0400

    feat: create use-track-position hook

---
+++ src/components/elements/modal/hooks/use-track-position.ts
@@ -0,0 +1,55 @@
+import { useState, useLayoutEffect } from "react";
+
+export const useTrackPosition = ({
+  buttonRef,
+  trackScroll = false,
+}: {
+  buttonRef: React.RefObject<HTMLButtonElement>;
+  trackScroll?: boolean;
+}) => {
+  const [buttonBoundaries, setButtonBoundaries] = useState<DOMRect | null>(
+    buttonRef?.current?.getBoundingClientRect() ?? null,
+  );
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      if (buttonRef?.current) {
+        setButtonBoundaries(buttonRef.current.getBoundingClientRect());
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    if (trackScroll) {
+      window.addEventListener("scroll", handleResize);
+    }
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      if (trackScroll) {
+        window.removeEventListener("scroll", handleResize);
+      }
+    };
+  }, [
+    buttonRef,
+    buttonRef.current?.getBoundingClientRect,
+    trackScroll,
+    setButtonBoundaries,
+  ]);
+
+  useLayoutEffect(() => {
+    setButtonBoundaries(buttonRef.current?.getBoundingClientRect() ?? null);
+  }, [
+    buttonRef,
+    buttonRef.current?.getBoundingClientRect,
+    setButtonBoundaries,
+  ]);
+
+  const style: React.CSSProperties = {
+    position: "fixed",
+    top: buttonBoundaries?.top,
+    left: buttonBoundaries?.left,
+    transform: `translateX(calc(-${100}% + ${buttonBoundaries?.width}px))`,
+  };
+
+  return style;
+};
