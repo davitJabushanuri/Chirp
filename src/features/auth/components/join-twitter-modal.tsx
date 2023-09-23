3cae088d3706fcbef49e44cc0c08b1035dc9beb3
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-09-23 20:42:51 +0400

    refactor: make join-twitter-modal accessible

--- src/features/auth/components/join-twitter-modal.tsx
+++ src/features/auth/components/join-twitter-modal.tsx
@@ -9,7 +9,6 @@ import { FollowIcon } from "@/assets/follow-icon";
 import { HeartIconActive } from "@/assets/heart-icon";
 import { MessageIcon } from "@/assets/message-icon";
 import { RetweetIcon } from "@/assets/retweet-icon";
-import { TwitterLogo } from "@/assets/twitter-logo";
 import { CloseButton } from "@/components/elements/close-button";
 
 import { useJoinTwitter } from "../stores/useJoinTwitter";
@@ -49,91 +48,85 @@ export const JoinTwitterModal = () => {
         </CloseButton>
       </div>
 
-      <div className={styles.wrapper}>
-        <div className={styles.icon}>
-          {data.action === `comment` ? (
-            <span className={styles.comment}>
-              <CommentIconFill />
-            </span>
-          ) : data.action === `retweet` ? (
-            <span className={styles.retweet}>
-              <RetweetIcon />
-            </span>
-          ) : data.action === `like` ? (
-            <span className={styles.like}>
-              <HeartIconActive />
-            </span>
-          ) : data.action === `follow` ? (
-            <span className={styles.follow}>
-              <FollowIcon />
-            </span>
-          ) : data.action === `message` ? (
-            <span className={styles.comment}>
-              <MessageIcon />
-            </span>
-          ) : (
-            <span className={styles.comment}>
-              <TwitterLogo />
-            </span>
-          )}
-        </div>
+      <div className={styles.icon}>
+        {data.action === `comment` ? (
+          <span className={styles.comment}>
+            <CommentIconFill />
+          </span>
+        ) : data.action === `retweet` ? (
+          <span className={styles.retweet}>
+            <RetweetIcon />
+          </span>
+        ) : data.action === `like` ? (
+          <span className={styles.like}>
+            <HeartIconActive />
+          </span>
+        ) : data.action === `follow` ? (
+          <span className={styles.follow}>
+            <FollowIcon />
+          </span>
+        ) : data.action === `message` ? (
+          <span className={styles.comment}>
+            <MessageIcon />
+          </span>
+        ) : null}
+      </div>
 
-        <div className={styles.contentWrapper}>
-          <div className={styles.content}>
-            <h1 className={styles.title}>
-              {data.action === `comment`
-                ? `Reply to join the conversation.`
-                : data.action === `retweet`
-                ? `Retweet to spread the word.`
-                : data.action === `like`
-                ? `Like a Tweet to share the love.`
-                : data.action === `follow`
-                ? `Follow ${data.user} to see what they share on Twitter.`
-                : data.action === `message`
-                ? `Join Twitter now so you can share The New European - Think Without Borders’s Tweet privately.`
-                : `Don’t miss what’s happening`}
-            </h1>
-            <p className={styles.subtitle}>
-              {data.action === `comment`
-                ? `Once you join Twitter, you can respond to ${data.user}’s Tweet.`
-                : data.action === `retweet`
-                ? `When you join Twitter, you can share ${data.user}’s Tweet with your followers.`
-                : data.action === `like`
-                ? `Join Twitter now to let ${data.user} know you like their Tweet.`
-                : data.action === `follow`
-                ? `Sign up so you never miss their Tweets.`
-                : `People on Twitter are the first to know.`}
-            </p>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          {data.action === `comment`
+            ? `Reply to join the conversation.`
+            : data.action === `retweet`
+            ? `Retweet to spread the word.`
+            : data.action === `like`
+            ? `Like a Tweet to share the love.`
+            : data.action === `follow`
+            ? `Follow ${data.user} to see what they share on Twitter.`
+            : data.action === `message`
+            ? `Join Twitter now so you can share The New European - Think Without Borders’s Tweet privately.`
+            : `Don’t miss what’s happening`}
+        </h1>
+        <p className={styles.subtitle}>
+          {data.action === `comment`
+            ? `Once you join Twitter, you can respond to ${data.user}’s Tweet.`
+            : data.action === `retweet`
+            ? `When you join Twitter, you can share ${data.user}’s Tweet with your followers.`
+            : data.action === `like`
+            ? `Join Twitter now to let ${data.user} know you like their Tweet.`
+            : data.action === `follow`
+            ? `Sign up so you never miss their Tweets.`
+            : `People on Twitter are the first to know.`}
+        </p>
 
-            <div className={styles.buttons}>
-              <button
-                onClick={() => {
-                  setJoinTwitterData({
-                    isModalOpen: false,
-                    action: "",
-                    user: "",
-                  });
-                  router.push(`/auth/signin`);
-                }}
-                className={styles.signin}
-              >
-                Log in
-              </button>
-              <button
-                onClick={() => {
-                  setJoinTwitterData({
-                    isModalOpen: false,
-                    action: "",
-                    user: "",
-                  });
-                  router.push(`/auth/signup`);
-                }}
-                className={styles.signup}
-              >
-                Sign up
-              </button>
-            </div>
-          </div>
+        <div className={styles.buttons}>
+          <button
+            role="link"
+            onClick={() => {
+              setJoinTwitterData({
+                isModalOpen: false,
+                action: "",
+                user: "",
+              });
+              router.push(`/auth/signin`);
+            }}
+            className={styles.signin}
+          >
+            Log in
+          </button>
+          <button
+            role="link"
+            onClick={() => {
+              setJoinTwitterData({
+                isModalOpen: false,
+                action: "",
+                user: "",
+              });
+              router.push(`/auth/signin`);
+            }}
+            className={styles.signup}
+          >
+            Sign up
+          </button>
         </div>
       </div>
     </motion.div>
