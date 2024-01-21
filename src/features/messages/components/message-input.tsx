98affba9564443bba5ee267edfca624492d1be4c
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-01-21 21:12:58 +0400

    feat: send messages in realtime

--- src/features/messages/components/message-input.tsx
+++ src/features/messages/components/message-input.tsx
@@ -1,3 +1,5 @@
+import { createId } from "@paralleldrive/cuid2";
+import { useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
 import { useRef, useState } from "react";
 
@@ -7,88 +9,152 @@ import { GifIcon } from "@/assets/gif-icon";
 import { ImageIcon } from "@/assets/image-icon";
 import { CloseButton } from "@/components/elements/close-button";
 import { IChosenImages } from "@/features/create-tweet";
+import { socket } from "@/lib/socket-io";
 
 import { SendIcon } from "../assets/send-icon";
-import { useCreateMessage } from "../hooks/use-create-message";
+import { IMessage } from "../types";
 
 import styles from "./styles/message-input.module.scss";
 
 export const MessageInput = ({
-  conversationId,
-  senderId,
-  receiverId,
+  conversation_id,
+  sender_id,
+  receiver_id,
 }: {
-  conversationId: string | undefined;
-  senderId: string | undefined;
-  receiverId: string | undefined;
+  conversation_id: string | undefined;
+  sender_id: string | undefined;
+  receiver_id: string | undefined;
 }) => {
   const [text, setText] = useState("");
   const imageUploadRef = useRef<HTMLInputElement>(null);
-  const [chosenImages, setChosenImages] = useState<IChosenImages[]>([]);
+  const [chosenImage, setChosenImage] = useState<IChosenImages | null>(null);
 
-  const mutation = useCreateMessage({
-    setText,
-    setChosenImages,
-  });
+  const queryClient = useQueryClient();
 
-  const chooseImage = async (event: any) => {
-    const file = event.target.files[0];
+  const chooseImage = (e: any) => {
+    const file = e.target.files[0];
 
-    // reset file input
     if (imageUploadRef.current) imageUploadRef.current.value = "";
 
     if (file) {
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = () => {
-        setChosenImages([
-          ...chosenImages,
-          {
-            url: reader.result,
-            id: Math.random(),
-            file: file,
-          },
-        ]);
+        setChosenImage({
+          url: reader.result,
+          id: Math.random(),
+          file: file,
+        });
       };
     }
   };
 
+  const onFocus = () => {
+    // detect if the last message is from the current user
+    const chat = queryClient.getQueryData<IMessage[]>([
+      "chat",
+      conversation_id,
+    ]);
+    if (!chat) return;
+    const lastMessage = chat.at(-1);
+
+    if (lastMessage?.sender_id === sender_id) return;
+
+    socket.emit("status", { status: "seen", message_id: lastMessage?.id });
+  };
+
+  const createMessage = ({
+    id,
+    text,
+    conversation_id,
+    sender_id,
+    receiver_id,
+    image,
+    file,
+    status,
+  }: {
+    id: string;
+    text: string;
+    conversation_id: string | undefined;
+    sender_id: string | undefined;
+    receiver_id: string | undefined;
+    image: string | ArrayBuffer | null;
+    file: File | null;
+    status: "sending" | "sent" | "seen" | "failed";
+  }) => {
+    return {
+      id,
+      text,
+      conversation_id,
+      sender_id,
+      receiver_id,
+      image,
+      file,
+      status,
+    };
+  };
+
+  const onSubmit = async () => {
+    const id = createId();
+
+    const message = createMessage({
+      id,
+      text,
+      conversation_id,
+      sender_id,
+      receiver_id,
+      image: chosenImage?.url ?? null,
+      file: chosenImage?.file ?? null,
+      status: "sending",
+    });
+
+    queryClient.setQueryData(["chat", conversation_id], (oldData: any) => {
+      return [...oldData, message];
+    });
+
+    socket.emit("message", {
+      id,
+      text,
+      image: chosenImage ? chosenImage.file : null,
+      conversation_id,
+      sender_id,
+      receiver_id,
+    });
+
+    setText("");
+    setChosenImage(null);
+  };
+
   return (
-    <div className={styles.container}>
-      {chosenImages?.length > 0 && (
+    <aside aria-label="Start a new message" className={styles.container}>
+      {chosenImage && (
         <div className={styles.mediaPreview}>
-          {chosenImages.map((image) => {
-            return (
-              <div key={image.id} className={styles.imageContainer}>
-                <CloseButton
-                  onClick={() => {
-                    setChosenImages(
-                      chosenImages.filter((img) => img.id !== image.id),
-                    );
-                  }}
-                  ariaLabel="Remove media"
-                  title="Remove"
-                >
-                  <CloseIcon />
-                </CloseButton>
-                <Image
-                  src={image.url as string}
-                  alt=""
-                  width={100}
-                  height={100}
-                />
-              </div>
-            );
-          })}
+          <div className={styles.imageContainer}>
+            <CloseButton
+              onClick={() => {
+                setChosenImage(null);
+              }}
+              ariaLabel="Remove media"
+              title="Remove"
+            >
+              <CloseIcon />
+            </CloseButton>
+            <Image
+              src={(chosenImage.url as string) ?? ""}
+              alt=""
+              width={100}
+              height={100}
+            />
+          </div>
         </div>
       )}
 
       <div
         className={`${styles.inputContainer} ${
-          chosenImages?.length > 0 ? styles.active : ""
+          chosenImage ? styles.active : ""
         }`}
       >
-        {chosenImages?.length <= 0 && (
+        {!chosenImage && (
           <div className={styles.actions}>
             <button
               type="button"
@@ -114,37 +180,26 @@ export const MessageInput = ({
           </div>
         )}
 
-        <div className={styles.input}>
-          <input
-            type="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder="Start a new message"
-          />
-        </div>
+        <input
+          onFocus={() => {
+            onFocus();
+          }}
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Start a new message"
+        />
 
-        <div className={styles.send}>
-          <button
-            type="submit"
-            onClick={() => {
-              mutation.mutate({
-                text: text,
-                files: chosenImages.map((img) => img.file),
-                conversationId: conversationId,
-                senderId: senderId,
-                receiverId: receiverId,
-              });
-            }}
-            disabled={
-              (text === "" && chosenImages.length <= 0) ||
-              mutation.status === "pending"
-            }
-            className={styles.icon}
-          >
-            <SendIcon />
-          </button>
-        </div>
+        <button
+          aria-label="Send"
+          type="submit"
+          onClick={onSubmit}
+          disabled={text === "" && !chosenImage}
+          className={styles.send}
+        >
+          <SendIcon />
+        </button>
       </div>
-    </div>
+    </aside>
   );
 };
