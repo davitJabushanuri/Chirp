9c0cd38b3d0c381ffe0f1436f819e77361fa8aa7
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-02-05 20:27:54 +0400

    feat: create scroll to bottom button

--- src/features/messages/components/conversation.tsx
+++ src/features/messages/components/conversation.tsx
@@ -3,10 +3,12 @@ import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
+import { Button } from "@/components/elements/button";
 import { LoadingSpinner } from "@/components/elements/loading-spinner";
 import { TryAgain } from "@/components/elements/try-again";
 import { socket } from "@/lib/socket-io";
 
+import { ArrowDownIcon } from "../assets/arrow-down-icon";
 import { useGetConversation } from "../hooks/use-get-conversation";
 
 import { Chat } from "./chat";
@@ -61,7 +63,7 @@ export const Conversation = () => {
         user_image={conversationMember?.profile_image_url}
         isVerified={conversationMember?.verified}
       />
-      <div id="chat-container" className={styles.conversation}>
+      <div id="chat-container" className={`${styles.conversation} relative`}>
         <ConversationMemberDetails user={conversationMember} />
         <Chat conversation_id={conversation?.id} />
         <div id="last-element" />
@@ -76,6 +78,10 @@ export const Conversation = () => {
           ↓ New messages
         </button>
       )}
+      <Button className="shadow-main absolute bottom-[5rem] right-[1.6rem] bg-background fill-primary-100 px-[1em] py-[0.5em] hover:bg-neutral-500 focus-visible:bg-neutral-500 focus-visible:ring-secondary-100/80 active:bg-neutral-600">
+        <ArrowDownIcon />
+      </Button>
+
       <MessageInput
         conversation_id={conversation?.id}
         sender_id={session?.user?.id}
