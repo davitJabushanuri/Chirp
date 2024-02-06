440a47f8296f8ab2b8a75a43a56b830ff37b3700
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-02-07 00:50:31 +0400

    refactor: tweet-optons to use Button component, add forwardRef to Button

--- src/components/elements/button/components/button.tsx
+++ src/components/elements/button/components/button.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { forwardRef } from "react";
 
 import { cn } from "@/utils/cn";
 
@@ -6,21 +6,26 @@ interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export const Button: FC<IButton> = ({ children, className, ...props }) => {
-  return (
-    <button
-      {...props}
-      className={cn(
-        "grid cursor-pointer place-items-center rounded-full",
-        "transition-all duration-200 ease-in-out",
-        "fill-secondary-100 p-[0.5em]",
-        "focus-visible:ring-2 focus-visible:ring-primary-100",
-        "fill-secondary-100 [&>svg]:w-h2",
-        "disabled-button",
-        className,
-      )}
-    >
-      {children}
-    </button>
-  );
-};
+export const Button = forwardRef<HTMLButtonElement, IButton>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        {...props}
+        className={cn(
+          "grid cursor-pointer place-items-center rounded-full",
+          "transition-all duration-200 ease-in-out",
+          "fill-secondary-100 p-[0.5em]",
+          "focus-visible:ring-2 focus-visible:ring-primary-100",
+          "fill-secondary-100 [&>svg]:w-h2",
+          "disabled-button",
+          className,
+        )}
+      >
+        {children}
+      </button>
+    );
+  },
+);
+
+Button.displayName = "Button";
