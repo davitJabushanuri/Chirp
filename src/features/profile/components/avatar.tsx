359c86382d18218498e4d46678c5108ff354f1b7
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-02-06 16:30:25 +0400

    refactor: avatar component to use tailwind

--- src/features/profile/components/avatar.tsx
+++ src/features/profile/components/avatar.tsx
@@ -1,16 +1,29 @@
 import Image from "next/image";
+import React from "react";
 
-import styles from "./styles/avatar.module.scss";
+import { cn } from "@/utils/cn";
 
-export const Avatar = ({ userImage }: { userImage: string | null }) => {
+interface IAvatar extends React.ImgHTMLAttributes<HTMLImageElement> {
+  userImage: string | null;
+  className?: string;
+  width?: number;
+  height?: number;
+}
+
+export const Avatar = ({ userImage, className, ...props }: IAvatar) => {
   return (
-    <div className={styles.container}>
+    <div
+      className={cn(
+        "relative aspect-square w-[calc(var(--tw-fs-kilo)+9px)] overflow-hidden rounded-full",
+        className,
+      )}
+    >
       <Image
+        {...props}
         src={userImage || `/user_placeholder.png`}
         alt="profile picture"
-        draggable={true}
         fill={true}
-        sizes="var(--avatar-size)"
+        className="block size-full object-cover"
       />
     </div>
   );
