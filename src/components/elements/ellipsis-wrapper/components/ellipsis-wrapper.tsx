24e70b46f59dd1c1d5e9d623369dabd9d51be712
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-02-06 18:19:00 +0400

    refactor: ellipsis-wrapper component to use tailwind

--- src/components/elements/ellipsis-wrapper/components/ellipsis-wrapper.tsx
+++ src/components/elements/ellipsis-wrapper/components/ellipsis-wrapper.tsx
@@ -1,12 +1,14 @@
-import styles from "./styles/ellipses-wrapper.module.scss";
-
 export const EllipsisWrapper = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
   return (
-    <div tabIndex={-1} aria-hidden={true} className={styles.container}>
+    <div
+      aria-hidden={true}
+      role="presentation"
+      className="grid grid-flow-col [&>*]:truncate"
+    >
       {children}
     </div>
   );
