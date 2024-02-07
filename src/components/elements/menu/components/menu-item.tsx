d141d7f4f15752322a6983fbe472379c209cfdaf
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-02-08 03:00:17 +0400

    refactor: update menu and menu-item components to use tailwind

--- src/components/elements/menu/components/menu-item.tsx
+++ src/components/elements/menu/components/menu-item.tsx
@@ -1,22 +1,33 @@
-import styles from "./styles/menu-item.module.scss";
+import { cn } from "@/utils/cn";
 
 export const MenuItem = ({
   onClick,
-  color = "white",
   children,
+  color,
+  className,
 }: {
   onClick: () => void;
-  color?: "white" | "red";
   children: React.ReactNode;
+  color?: "red" | "white";
+  className?: string;
 }) => {
   return (
     <button
-      style={{
-        color: color === "white" ? "var(--clr-secondary)" : "var(--clr-red)",
-      }}
       onClick={onClick}
       role="menuitem"
-      className={styles.container}
+      className={cn(
+        "flex items-center gap-3 px-[1em] py-[0.75em] text-milli font-semibold text-secondary-100",
+        "hover:bg-neutral-200",
+        "outline-offset-[-2px] focus-visible:bg-neutral-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-primary-100",
+        "active:bg-neutral-300",
+        "first-of-type:rounded-t-[0.8rem] last-of-type:rounded-b-[0.8rem]",
+        "transition-colors duration-100 ease-in-out",
+        color === "red" && "text-red-100",
+        "fill-secondary-100",
+        color === "red" && "fill-red-100",
+        "[&>svg]:w-h3",
+        className,
+      )}
     >
       {children}
     </button>
