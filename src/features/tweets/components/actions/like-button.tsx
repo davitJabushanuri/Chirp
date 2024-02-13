4bce22ff53517001eb240480a8919ab655b1772b
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-02-13 17:41:35 +0400

    refactor: like, retweet and share buttons to use tailwind

--- src/features/tweets/components/actions/like-button.tsx
+++ src/features/tweets/components/actions/like-button.tsx
@@ -1,12 +1,13 @@
 import { useSession } from "next-auth/react";
 
 import { HeartIcon, HeartIconActive } from "@/assets/heart-icon";
+import { Button } from "@/components/elements/button";
+import { Tooltip } from "@/components/elements/tooltip";
 import { useJoinTwitter } from "@/features/auth";
+import { cn } from "@/utils/cn";
 
 import { useLike } from "../../hooks/use-like";
 import { ITweet } from "../../types";
-
-import styles from "./styles/actions.module.scss";
 
 export const LikeButton = ({
   tweet,
@@ -27,38 +28,46 @@ export const LikeButton = ({
   const mutation = useLike();
 
   return (
-    <button
-      aria-label={hasLiked ? "Unlike" : "Like"}
-      data-title={hasLiked ? "Unlike" : "Like"}
-      tabIndex={0}
-      onKeyDown={(e) => {
-        e.stopPropagation();
-      }}
-      onClick={(e) => {
-        e.stopPropagation();
-        if (!session) {
-          setJoinTwitterData({
-            isModalOpen: true,
-            action: "like",
-            user: tweet?.author?.name || "user",
-          });
-        }
-        mutation.mutate({ tweetId: tweet?.id, userId: session?.user?.id });
-      }}
-      className={`${styles.container} ${styles.like} ${
-        hasLiked ? styles.liked : ""
-      } `}
-    >
-      <span
-        className={`${styles.icon} ${
-          smallIcons ? styles.smallIcon : styles.bigIcons
-        }`}
+    <Tooltip text={hasLiked ? "Unlike" : "Like"}>
+      <Button
+        aria-label={hasLiked ? "Unlike" : "Like"}
+        onKeyDown={(e) => {
+          e.stopPropagation();
+        }}
+        onClick={(e) => {
+          e.stopPropagation();
+          if (!session) {
+            setJoinTwitterData({
+              isModalOpen: true,
+              action: "like",
+              user: tweet?.author?.name || "user",
+            });
+          }
+          mutation.mutate({ tweetId: tweet?.id, userId: session?.user?.id });
+        }}
+        className="group flex gap-[2px] p-0 focus-visible:outline-0"
       >
-        {hasLiked ? <HeartIconActive /> : <HeartIcon />}
-      </span>
-      {showStats && tweet?.likes && tweet?.likes?.length > 0 && (
-        <span className={styles.stats}>{tweet?.likes?.length}</span>
-      )}
-    </button>
+        <span
+          className={cn(
+            "rounded-full fill-tertiary-100 p-2 group-hover:bg-rose-100/20 group-hover:fill-rose-100  group-active:bg-rose-100/25 group-active:fill-rose-100",
+            "outline-offset-[-2px] group-focus-visible:bg-rose-100/20 group-focus-visible:fill-rose-100 group-focus-visible:outline group-focus-visible:outline-2 group-focus-visible:outline-rose-100",
+            "transition-colors duration-200 ease-in-out",
+            hasLiked && "fill-rose-100",
+          )}
+        >
+          {hasLiked ? <HeartIconActive /> : <HeartIcon />}
+        </span>
+        {tweet && tweet?._count?.likes > 0 && (
+          <span
+            className={cn(
+              "text-nano text-tertiary-100 group-hover:text-rose-100 group-focus-visible:text-rose-100 group-active:text-rose-100",
+              hasLiked && "text-rose-100",
+            )}
+          >
+            {tweet?._count?.likes}
+          </span>
+        )}
+      </Button>
+    </Tooltip>
   );
 };
