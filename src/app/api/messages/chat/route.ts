f796b21ecbe0017a5dd09667eaf28b82f8469e81
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-03-09 19:18:49 +0400

    refactor: chat API endpoint to support infinite scroll

--- src/app/api/messages/chat/route.ts
+++ src/app/api/messages/chat/route.ts
@@ -6,6 +6,11 @@ import { prisma } from "@/lib/prisma";
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const conversation_id = searchParams.get("conversation_id") as string;
+  const cursorQuery = searchParams.get("cursor") || undefined;
+  const take = Number(searchParams.get("limit")) || 20;
+
+  const skip = cursorQuery ? 1 : 0;
+  const cursor = cursorQuery ? { id: cursorQuery } : undefined;
 
   const messageSchema = z.string();
   const zod = messageSchema.safeParse(conversation_id);
@@ -16,12 +21,20 @@ export async function GET(request: Request) {
 
   try {
     const chat = await prisma.message.findMany({
+      skip,
+      take,
+      cursor,
       where: {
         conversation_id: conversation_id,
       },
     });
 
-    return NextResponse.json(chat, { status: 200 });
+    const nextId = chat.length < take ? undefined : chat[chat.length - 1].id;
+
+    return NextResponse.json({
+      chat,
+      nextId,
+    });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
