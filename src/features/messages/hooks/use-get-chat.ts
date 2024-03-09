ee2314bc078fa400fbb0c178462eeabb854e4796
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-03-09 19:20:10 +0400

    refactor: use-get-chat hook and get-chat function to support infinite scroll

--- src/features/messages/hooks/use-get-chat.ts
+++ src/features/messages/hooks/use-get-chat.ts
@@ -1,13 +1,31 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import { getChat } from "../api/get-chat";
 import { IMessage } from "../types";
 
+interface IInfiniteChat {
+  nextId: string;
+  chat: IMessage[];
+}
+
 export const useChat = (conversation_id: string | undefined) => {
-  return useQuery<IMessage[]>({
+  const response = useInfiniteQuery<IInfiniteChat>({
     queryKey: ["chat", conversation_id],
-    queryFn: async () => {
-      return getChat(conversation_id);
+    queryFn: ({ pageParam }) => {
+      return getChat({ conversation_id, pageParam, limit: 20 });
+    },
+    initialPageParam: "",
+    getNextPageParam: (lastPage) => {
+      return lastPage?.nextId;
     },
   });
+
+  return {
+    data: response.data?.pages.flatMap((page) => page.chat),
+    isLoading: response.isLoading,
+    isError: response.isError,
+    isFetchingNextPage: response.isFetchingNextPage,
+    fetchNextPage: response.fetchNextPage,
+    hasNextPage: response.hasNextPage,
+  };
 };
