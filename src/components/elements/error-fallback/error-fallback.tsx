e2f5901872ecde9acbc118c060c57de6be0aa0a9
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-03-31 23:16:11 +0400

    feat: create global-error component to catch errors in the main layout

---
+++ src/components/elements/error-fallback/error-fallback.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { FC } from "react";
+
+import { Button } from "../button";
+import { ReloadIcon } from "../try-again/assets/reload-icon";
+
+interface IErrorFallback {
+  error: Error;
+  resetErrorBoundary: () => void;
+}
+
+export const ErrorFallback: FC<IErrorFallback> = ({
+  error,
+  resetErrorBoundary,
+}) => {
+  return (
+    <div className="fixed inset-0 z-fixed flex flex-col items-center justify-center">
+      <p className="mb-5 text-milli text-tertiary-100">{error.message}</p>
+      <Button
+        className="flex gap-2 bg-primary-100 px-4 text-milli font-semibold outline-offset-2 hover:bg-primary-200 active:bg-primary-300"
+        onClick={resetErrorBoundary}
+      >
+        <ReloadIcon />
+        Retry
+      </Button>
+    </div>
+  );
+};
