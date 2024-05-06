b7f23f6e00908c8c404acde9caf6a9b24cf160a4
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-05-06 22:54:42 +0400

    feat: add admin and logs routes to admin navbar

--- src/app/admin/admin-navbar.tsx
+++ src/app/admin/admin-navbar.tsx
@@ -1,3 +1,33 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/utils/cn";
+
 export const AdminNavbar = () => {
-  return <div>admin navbar</div>;
+  const pathname = usePathname();
+  const path = pathname.split("/").at(-1);
+
+  return (
+    <nav aria-label="Admin" className="flex gap-4">
+      <Link
+        className={cn(
+          "cursor-pointer font-medium",
+          path === "admin" && "font-bold",
+        )}
+        href="/admin"
+      >
+        Dashboard
+      </Link>
+      <Link
+        className={cn(
+          "cursor-pointer font-medium",
+          path === "logs" && "font-bold",
+        )}
+        href="/admin/logs"
+      >
+        Logs
+      </Link>
+    </nav>
+  );
 };
